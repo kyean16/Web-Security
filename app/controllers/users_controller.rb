0e4e9af8b28671c15951e37fb8815edd598fd10a
keyean <kevinyean_2016@depauw.edu>
2015-04-28 21:52:01 -0400

    Finished Worksheet8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+
   def new
+  	@user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+    else
+       flash[:danger] = "Failure to sign up, please try again"
+       render 'new'
+       end
+   end
+
+  	private
+	   def user_params
+     		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+#this method only permits the name, email, password and password confirmation #attributes to pass. It raises an error if the :user attribute is missing.
+   		end
 end
