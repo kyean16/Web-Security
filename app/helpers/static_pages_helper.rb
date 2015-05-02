8f2df75f0b9bc81f69da0f25b752fa38388ada06
keyean <kevinyean_2016@depauw.edu>
2015-05-01 21:18:51 -0400

    Added Static Pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,18 @@
 module StaticPagesHelper
+
+	#Return a speficic Stat depending on the number num. Returns sentence
+	def randomStat(num)
+		case num
+		when 1
+			"Total Number of Books:"
+		when 2
+			"Total Number of Users:"
+		when 3
+			"Average Number of Books read by Users:"
+		when 4
+			"Favorite genre of book:"
+		else
+			sentence = "One"
+		end
+	end
 end
